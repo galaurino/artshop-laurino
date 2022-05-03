d027488b8f0ad4673d22f9760733e96c9e6943e1
Giuliana Laurino <laurino.giuliana@gmail.com>
2022-05-03 19:31:06 -0300

    NavBar

---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.css';
+
+function NavBar() {
+	return (
+
+			<div className="Nav">
+				<h1>WildArts</h1>
+				<ul>
+					<li><a href="#">Inicio</a></li>
+					<li><a href="#">Comisiones</a></li>
+					<li><a href="#">Remeras</a></li>
+					<li><a href="#">Posters</a></li>
+					<li><a href="#">Tazas</a></li>
+				</ul>
+			</div>
+
+		);
+}
+
+export default NavBar;
