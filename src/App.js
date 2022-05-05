14ba564506f7f5fb3c071d897ec79b8001eec530
Giuliana Laurino <laurino.giuliana@gmail.com>
2022-05-05 20:39:34 -0300

    CartWidget e ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/navBar/NavBar.js';
+import ItemListContainer from './components/itemListContainer/ItemListContainer.js';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting={"Productos van aquí."} />
     </div>
   );
 }
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,16 +1,19 @@
 import './NavBar.css';
+import CartWidget from '../cartWidget/CartWidget.js';
 
 function NavBar() {
 	return (
 
 			<div className="Nav">
-				<h1>WildArts</h1>
 				<ul>
+					<li><h1>WildArts</h1></li>
 					<li><a href="#">Inicio</a></li>
 					<li><a href="#">Comisiones</a></li>
 					<li><a href="#">Remeras</a></li>
 					<li><a href="#">Posters</a></li>
 					<li><a href="#">Tazas</a></li>
+					<li><a href="#">Contacto</a></li>
+					<li><CartWidget /></li>
 				</ul>
 			</div>
 
